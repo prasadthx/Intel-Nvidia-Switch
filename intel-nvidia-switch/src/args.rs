d2fecdf9c0f9fd14ee558c452f02508f07e7dc50
prasadthx <prasadzore31@gmail.com>
2022-12-03 23:59:13 +0530

    Added extra CLI Option

--- intel-nvidia-switch/src/args.rs
+++ intel-nvidia-switch/src/args.rs
@@ -2,9 +2,11 @@ use crate::graphics::GraphicsMode;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
+#[command(about,version)]
 pub struct Args {
     /// Choose the Graphics Mode: Nvidia or Intel or Hybrid.
     #[arg(long)]
+    #[arg(short='S')]
     pub switch: GraphicsMode,
 }
 
