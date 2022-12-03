63b9451d912c6f2b4b02ef84ee74df23fc5c27a4
prasadthx <prasadzore31@gmail.com>
2022-12-03 22:38:44 +0530

    Added CLI Parsing

--- intel-nvidia-switch/src/main.rs
+++ intel-nvidia-switch/src/main.rs
@@ -1,10 +1,14 @@
 use clap::Parser;
 mod graphics;
+use graphics::GraphicsMode;
 mod args;
 
 use args::Args;
 
 fn main() {
     let args = Args::parse();
-    println!("{:?}", args);   
+    match args.switch{
+        GraphicsMode::Nvidia => {println!("Nvidia");},
+        _ => {println!("{:?}", args);    },
+    }
 }
