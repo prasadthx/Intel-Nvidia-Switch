b4b795f84508301f3ce59db676da493a3392f899
prasadthx <prasadzore31@gmail.com>
2022-12-04 17:37:15 +0530

    Added configuration for Hybrid Mode

--- intel-nvidia-switch/src/switch.rs
+++ intel-nvidia-switch/src/switch.rs
@@ -1,4 +1,4 @@
-use std::fs::copy;
+use std::fs::{copy, remove_file};
 
 pub fn switch_intel() {
     println!("Switching to Intel");
@@ -18,4 +18,39 @@ pub fn switch_intel() {
         }
         Result::Err(x) => {println!("Error in copying file: {}", x)}
     }
+}
+
+pub fn switch_hybrid() {
+    println!("Switching to Hybrid...");
+    
+    let switch = true;
+    
+    if let Result(remove_intel_config) = remove_file("/etc/modprobe.d/blacklist-nvidia.conf"){
+        println!("Removed Nvidia modules blacklisting");
+    }
+    if let remove_nvidia_udev = remove_file("/etc/udev/rules.d/00-remove-nvidia.rules"){
+        println!("Removed Nvidia Shutdown Udev Rules");
+    }
+    
+    let nvidia_pm_mod = copy("./configs/hybrid/nvidia-pm.conf", "/etc/modprobe.d/nvidia-pm.conf") ;
+    match blacklist_nvidia {
+        Result::Ok(_x) => {
+            println!("Successfully added Configuration for Nvidia Dynamic Power Management");
+        },
+        Result::Err(x) => {println!("Error in copying file: {}", x); switch = false;}
+    }
+    
+    let shutdown_nvidia = copy("./configs/intel/80-nvidia-pm.rules", "/etc/udev/rules.d/80-nvidia-pm.rules") ;
+    match shutdown_nvidia {
+        Result::Ok(_x) => {
+            println!("Successfully added Configuration for Nvidia PM");
+        }
+        Result::Err(x) => {println!("Error in copying file: {}", x); switch = false;}
+    }
+    
+    if switch {
+        println!("Successfully Switched to Hybrid Mode!");
+        println!("Run Any Program On Nvidia GPU using --run flag.");
+    }
+    
 }
